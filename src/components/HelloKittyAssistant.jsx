4ac36a0b58c37787a9364f6036c22d39eb1c6b15
Oleh Basystyi <basystyo@gmail.com>
2025-05-07 02:14:10 +0300

    aniamtion

---
+++ src/components/HelloKittyAssistant.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/helloKittyAssistant.scss";
+import helloKittyLogo from "../assets/hello-kitty.png";
+
+const HelloKittyAssistant = ({ isThinking }) => {
+  return (
+    <div className={`hello-kitty-assistant ${isThinking ? 'thinking' : 'idle'}`}>
+      <img 
+        src={helloKittyLogo} 
+        alt="Hello Kitty Assistant" 
+        className="hello-kitty-image"
+      />
+      {isThinking && (
+        <div className="thinking-indicator">
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HelloKittyAssistant;
