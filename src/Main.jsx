4519995dee397f27e0a3e01747392b80376790e4
Anya Stasyshyn <anna.stasyshynn@gmail.com>
2025-05-18 21:30:55 +0300

    add toasts back

--- src/Main.jsx
+++ src/Main.jsx
@@ -8,12 +8,14 @@ import Login from "./components/Login.jsx";
 import Title from "./components/Title.jsx";
 import History from "./components/History.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import ToastContainer from "./components/ToastContainer.jsx";
 
 import "./utils/authHelpers.js";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
+            <ToastContainer />
             <Routes>
                 <Route path="/" element={<Title />} />
                 <Route path="/login" element={<Login />} />
